54a8e62f5e27018d40ffdb49178703c2b9fe2929
annyeong-coder <annyeong_edu@protonmail.com>
2022-11-06 19:54:20 +0900

    staging all files

--- LCR_main_code.R
+++ LCR_main_code.R
@@ -1,1 +1,5 @@
 # Info: this is the main R script file for my LCR final project on L2 development and future constructions.
+
+tempfull <- full_uncleaned_PELIC_TEMP01_0_221105_1337
+tempfull
+head(tempfull)
