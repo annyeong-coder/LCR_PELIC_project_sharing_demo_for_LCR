0ecf0739440bacf7e7d6c1dac6062b7c4005569a
annyeong-coder <annyeong_edu@protonmail.com>
2022-11-06 17:02:25 +0900

    CLI fix for making sure Rstudio doesn't hang when committing large files. Still need to "Push" afterwards.

---
+++ _CLI_fix_for_committing_large_files.R
@@ -0,0 +1,14 @@
+# Of course you can ignore the GUI and stick to the command-line Git forever, but if you don't want, a quick jump to the command-line git would solve this problem for now.
+
+# The temporary solution that I found is as follows:
+
+#    Click on the blue-gear icon on the GIT panel, inside RStudio.
+#    Select Shell (a terminal window will pup up!)
+#    Write the add and commit command in the terminal:
+
+# {ATTENTION: The following command will commit changes on ALL files! You may want to use what is appropriate for your situation!}
+
+#    git add -A && git commit -m 'staging all files'
+
+# Now you can go back to the GUI Git, and click on push button. All files that you staged in the terminal window, will be pushed up to your repository.
+
